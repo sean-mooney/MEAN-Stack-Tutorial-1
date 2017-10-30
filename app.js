02f0a65c411b1c89390ae857f66cce658610d56b
Sean Mooney <smooney914@gmail.com>
2017-10-30 16:27:57 -0400

    New Posts, Upvotes, Downvotes, Links

--- app.js
+++ app.js
@@ -8,4 +8,25 @@ app.controller('MainCtrl', ['$scope', function($scope){
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
 ];
+  $scope.addPost = function(){
+    if (!$scope.title || $scope.title === ''){
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0});
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  }
+
+  $scope.decrementUpvotes = function(post) {
+    if(post.upvotes > 0){ //Can't have negative points
+      post.upvotes -= 1;
+    }
+  }
 }]);
